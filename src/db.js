9f41f8d17da7f892293911840d6a096fbd010984
Michael Crawbuck <michael@brooklinen>
2023-06-08 16:36:03 -0400

    commands, and tests

--- src/db.js
+++ src/db.js
@@ -1,6 +1,17 @@
 import fs from "node:fs/promises";
 const DB_PATH = new URL("../db.json", import.meta.url).pathname;
 
+export const insertDB = async (note) => {
+  try {
+    const db = await getDB();
+    db.notes.push(note);
+    await saveDB(db);
+    return note;
+  } catch (err) {
+    return (err);
+  }
+};
+
 export const getDB = async () => {
   try {
     const db = await fs.readFile(DB_PATH, "utf-8");
@@ -14,17 +25,6 @@ export const saveDB = async (db) => {
   try {
     await fs.writeFile(DB_PATH, JSON.stringify(db, null, 2));
     return db;
-  } catch (err) {
-    return (err);
-  }
-};
-
-export const insertDB = async (note) => {
-  try {
-    const db = await getDB();
-    db.notes.push(note);
-    await saveDB(db);
-    return note;
   } catch (err) {
     return (err);
   }
